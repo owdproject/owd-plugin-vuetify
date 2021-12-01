ba10a2656f64b6e6be038436d9555837b2e317dc
hacklover <info@hacklover.net>
2021-12-01 05:55:44 +0100

    Minor improvements

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,5 @@ import { createVuetify } from 'vuetify/dist/vuetify.js'
 import 'vuetify/dist/vuetify.min.css';
 
 export default function(app: App) {
-  const vuetify = createVuetify(app.config.globalProperties.$owd.vuetify)
-
-  app.use(vuetify)
+  app.use(createVuetify(app.config.globalProperties.$owd.vuetify))
 }
